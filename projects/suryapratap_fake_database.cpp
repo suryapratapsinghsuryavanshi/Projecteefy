372e84204aabb2fb4501af0cdf1ecb284e9a8297
Suryapratap Singh Suryavanshi <suryprtaps@gmail.com>
2023-01-07 10:44:00 +0530

    add basic code of fake_db

---
+++ projects/suryapratap_fake_database.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+// Linked List Block
+class LinkedList {
+    public:
+        string data;
+        LinkedList *next;
+
+    LinkedList(string d, LinkedList *n) {
+        data = d;
+        next = n;
+    }
+
+    // Database Read Operation
+    void read(LinkedList *head) {
+        LinkedList *t = head;
+        int nm = 0;
+        do {
+            nm++;
+            cout<<"| "<<nm<<" | "<<t -> data<<" |"<<endl;
+            t = t -> next;
+        }while(t != NULL);
+    }
+
+    // Database Write Operation
+    LinkedList* write(LinkedList *head, string data) {
+        LinkedList *temp = new LinkedList(data, NULL);
+        if(head == NULL) {
+            head = temp;
+        }else {
+            LinkedList *t = head;
+            while(t -> next != NULL){
+                t = t -> next;
+            }
+            t -> next = temp;
+        }
+        return head;
+    }
+
+    // Database Search Operation
+    void search(LinkedList *head, int idx) {
+        LinkedList *t = head;
+        int nm = 0;
+        do {
+            nm++;
+            if(idx == nm) {
+                cout<<"Searched: | "<<nm<<" | "<<t -> data<<" |"<<endl;
+            }
+            t = t -> next;
+        }while(t != NULL);
+    }
+};
+
+// File Handling
+
+int main() {
+    // Create a linkedlist.
+    LinkedList *head = NULL;
+    head = head->write(head, "One");
+    head = head->write(head, "Two");
+    head->read(head);
+    head->search(head, 1);
+}
